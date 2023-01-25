a3cb3d3f7a2b92fe49b97b5bdf70df95f02a58e6
Mishael <mishael.hussain@tktk.ee>
2023-01-25 22:35:48 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Rakendus küsib kasutajal sisestada tema nime 
+//Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:");
+//string- sõne
+string UserName = Console. ReadLine();
+
+//string interpolation 
+Console.WriteLine($"Hello, {UserName} !"); 
+
+Console.WriteLine("Enter your password");
+string Password = Console.ReadLine();
